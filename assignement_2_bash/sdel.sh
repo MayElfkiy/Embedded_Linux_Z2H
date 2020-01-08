5a52bb3455b050a81fd47719f1f85f2f833f740e
May Alaa <mayelfkiy@gmail.com>
2020-01-08 18:11:02 +0200

    Initiaaall

---
+++ assignement_2_bash/sdel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+if [ -e ~/TRASH ] 	#checks if the directory exists
+then
+  :
+else 
+  mkdir -p ~/TRASH	#create the directory
+  chmod 777 ~/TRASH	
+  (crontab -l 2>/dev/null; echo "*/30 * * * * sdel.sh") | crontab -  #adding the script to cron to run automatically
+fi
+
+find ~/TRASH -type f -mtime +2 -exec rm -f {} \; 	#remove files older than 2 days
+
+for fileN in "$@"
+do
+if file $fileN* | grep -q "compressed data"; then 	#check whether the file is already compressed
+	echo "file $fileN is already zipped" 
+else
+	gzip $fileN					#compress file
+fi
+mv $fileN* ~/TRASH					#move file to trash
+done
